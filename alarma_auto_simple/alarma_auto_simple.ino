12d0e9ab485a390c779a379d9db3e5cf8694bcba
lolaszw <49764391@est.ort.edu.ar>
2026-07-04 14:36:35 -0300

    Add files via upload

---
+++ alarma_auto_simple/alarma_auto_simple.ino
@@ -0,0 +1,52 @@
+
+#define botonAlarma 2;
+#define ledAlarma 13;
+
+enum Alarma {
+  APAGADA,
+  CONTANDO_TIEMPO,
+  ACTIVADA
+};
+Alarma estadoAlarma = APAGADA;
+unsigned long tiempo = 0;
+unsigned long tiempoAhora = 0;
+
+void setup() {
+  pinMode(botonAlarma, INPUT_PULLUP);
+  pinMode(ledAlarma, OUTPUT);
+  digitalWrite(ledAlarma, LOW);  //siempre empieza apagado
+  Serial.begin(9600);
+}
+
+void loop() {
+  int boton = digitalRead(botonAlarma);
+  tiempoAhora = millis();
+
+  switch (estadoAlarma) {
+
+    case APAGADA:
+      if (boton == LOW) {
+        tiempo = tiempoAhora;  //empieza a contar desde ahora 
+        estadoAlarma = CONTANDO_TIEMPO;
+      }
+      break;
+
+    case CONTANDO_TIEMPO:
+      if (boton == HIGH) {  // Si sueltan el boton, se cancela la espera
+        estadoAlarma = APAGADA;
+      } else if (tiempoAhora - tiempo >= 2000) {  // si pasaron 2 segundos con el boton apretado, se prende la alarma
+        estadoAlarma = ACTIVADA;
+        Serial.println("Alarma activada");
+      }
+      break;
+
+    case ALARMA_ACTIVADA:
+      digitalWrite(ledAlarma, HIGH);
+      if (boton == HIGH) {  //si sueltan el boton se apaga la alarma
+        digitalWrite(ledAlarma, LOW);
+        estadoAlarma = APAGADA;
+        Serial.println("Alarma apagada");
+      }
+      break;
+  }
+}
