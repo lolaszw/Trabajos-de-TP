44c2a2bbe4b0fc58977c320a79c91fe67c1b9454
lolaszw <49764391@est.ort.edu.ar>
2026-07-04 10:54:13 -0300

    Add files via upload

--- cafetera/cafetera.ino
+++ cafetera/cafetera.ino
@@ -1,12 +1,17 @@
-enum cafetera { elegir,
-                servir,
-                dispensando,
-                retirar };
-cafetera estadoActual = elegir;
+#include <Arduino.h>
+#include <AsciiMassage.h>
+
+enum cafetera { ELEGIR,
+                SERVIR_LARGO,
+                SERVIR_CORTO,
+                RETIRAR };
+cafetera estadoActual = ELEGIR;
 
 #define botonInicio 3
 #define ledSirviendo 4
 #define sensorCafe 5
+
+String cafeIngresado = "";
 
 unsigned long tiempo = 0;
 
@@ -18,19 +23,50 @@ void setup() {
 }
 
 void loop() {
-  switch (cafetera) {
-    case (elegir):
+  switch (estadoActual) {
+    case (ELEGIR):
       {
-        int cafeElegido = Serial.write();
-        if (botonInicio == LOW) {
-          estadoActual = servir;
+        if (Serial.available() > 0) {
+          cafeIngresado = Serial.readStringUntil('\n');
+          cafeIngresado.trim();
+        }                                                      //ingresar que tipo de cafe queres
+        if (digitalRead(botonInicio) == LOW && cafeIngresado == "corto") {  // cuando presionas el boton empieza a servirse
+          estadoActual = SERVIR_CORTO;
           tiempo = millis();
-      }
-    case (servir):
-      {
-        if (cafeElegido == corto) {
-          digitalWrite(ledSirviendo, HIGH);
+          cafeIngresado = "";
+        } else if (digitalRead(botonInicio) == LOW && cafeIngresado == "largo") {
+          estadoActual = SERVIR_LARGO;
+          tiempo = millis();
+          cafeIngresado = "";
         }
+        break;
+      }
+    case (SERVIR_CORTO):
+      {
+
+        digitalWrite(ledSirviendo, HIGH);  //muestra que estan sirviendo el cafe
+        if (millis() - tiempo >= 3000) {   //si el cafe seleccionado es corto, se sirve por tres segundos
+          estadoActual = RETIRAR;
+        }
+
+
+        break;
+      }
+    case (SERVIR_LARGO):
+      {
+        digitalWrite(ledSirviendo, HIGH);
+        if (millis() - tiempo >= 5000) {  //si el cafe seleccionado es largo, se sirve por cinco segundos
+          estadoActual = RETIRAR;
+        }
+        break;
+      }
+    case (RETIRAR):
+      {
+        digitalWrite(ledSirviendo, LOW);       //la cafetera dejo de servir, por ende el led se apaga
+        if (digitalRead(sensorCafe) == LOW) {  // el sensor dejo de ver al cafe (lo retiraron), y se reinicia el ciclo
+          estadoActual = ELEGIR;
+        }
+        break;
       }
   }
 }
